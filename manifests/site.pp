c314f67d506889bd1d8f48cc60ae7f6582706536
Nam Sangyun <namsangyun@linuxmaster.com>
2015-07-25 14:12:00 +0900

    Added file resource to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,8 +2,14 @@ node 'namsang-ko.linuxmaster.com' {
 #   content => "Hello, puppet master!\n",
 }
 
+node 'vm01.linuxmaster.com' {
+    file { '/tmp/testserver' :
+        content => "This is a test server.\n",
+    }
+}
+
 node /^vm[0-9]+\.linuxmaster\.com$/ {
-    include vim_upgrade
-    include sshd
+#   include vim_upgrade
+#   include sshd
 }
 
